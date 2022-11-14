c6cc7a0c5f331ff97bfe406ddcc141963d454c90
t8210103 <115114218+t8210103@users.noreply.github.com>
2022-11-14 20:11:07 +0200

    added number inserted by user to determine wanted action

--- iasonasFirstDir/Post_code.java
+++ iasonasFirstDir/Post_code.java
@@ -1,5 +1,10 @@
-import java.util.scanner
+import java.util.Scanner;
 
 public class Post_code {
-
+	public static int num_of_choice () {
+		Scanner input = new Scanner(System.in);
+		System.out.println("click 1 to create a post" + "\n" + "click 2 to delete an owned post" + "\n" + "click 3 to edit an owned post");
+		int number = input.nextInt();
+   	 	return number;
+	}
 }
