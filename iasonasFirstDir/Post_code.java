9214c1b21b3721b577c128d5890ec19035af8870
t8210103 <115114218+t8210103@users.noreply.github.com>
2022-11-14 20:31:37 +0200

    improved Post_code

--- iasonasFirstDir/Post_code.java
+++ iasonasFirstDir/Post_code.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
 
 public class Post_code {
-	public static int num_of_choice () {
+	public num_of_choice () {
 		Scanner input = new Scanner(System.in);
 		System.out.println("click 1 to create a post" + "\n" + "click 2 to delete an owned post" + "\n" + "click 3 to edit an owned post");
 		int number = input.nextInt();
-   	 	return number;
+   	 	if (number == 1) {
+			Create_post.some_method();
+		}else if (number == 2); {
+			Delete_post.some_method2();
+		}else{
+			Edit_post.some_method3();
+		}
 	}
 }
