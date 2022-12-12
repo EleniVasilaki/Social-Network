fb55df8027b1c5baef6a3ccd3d64215fbbd17b9c
t8210103 <115114218+t8210103@users.noreply.github.com>
2022-12-12 20:30:40 +0200

    made somee changes

--- iasonasFirstDir/Create_post.java
+++ iasonasFirstDir/Create_post.java
@@ -1,34 +1,59 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-// H upload na kanei extend thn create: mesv methodoy ths create na kalv thn upload analoga ean ebale erigrafh h oxi o xrhsths????
-public  class Create_post extends Post_code{
-    public void createPostMethod() {
+// H upload na kanei extend thn create: mesw methodou ths create na kalw thn upload analoga ean ebale perigrafh h oxi o xrhsths????
+public  class Create_Post extends Post_code{
+	//public static String path;
+    public static Path createPostMethod() { //return is for testing purposes
         Scanner in = new Scanner(System.in);
-        System.out.println("click 1 to add a description to your post" + "\n" + "click 2 to add text/link for your post"); // more options
+        System.out.println("click 1 to add a description to your post" + "\n" + "click 2 to add text/link for your post"); // more options?
         int option = in.nextInt();
-        if (option == 1) {
-            System.out.println("Please enter your description:");
-            String description = in.next();
-            try {
-                BufferedWriter writer = new BufferedWriter( new FileWriter(".\\post.txt"));
-                writer.write(description);
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } 
-        }
-        System.out.println("Please insert link:");
-        String userPost = in.next();
         try {
-            BufferedWriter writer = new BufferedWriter( new FileWriter(".\\post.txt"));
-            writer.write(userPost);
-            writer.close();
-            } catch (IOException e) {
-            e.printStackTrace();
-        } 
-        in.close();
+			BufferedWriter writer = new BufferedWriter( new FileWriter(".\\post.txt", true));
+			BufferedReader reader = new BufferedReader(new FileReader(".\\post.txt"));
+			long postId = 0;
+			String line;
+			while ((line = reader.readLine()) != null) {
+				postId = postId + 1;
+				System.out.println(postId);
+			}
+			postId = postId / 3; //Otherwise postId goes up by 3 not by 1
+			System.out.println(postId);
+			in.nextLine();
+			if (option == 1) {
+				writer.write(postId + "\n");
+			    System.out.println("Please enter your description:");
+			    String description = in.nextLine();
+			    writer.write(description + "\n");
+				System.out.println("Please insert link:");
+			    String userPost = in.nextLine();
+				writer.write(userPost + "\n");
+				writer.close();
+				in.close();
+			} else if (option == 2) {
+				writer.write(postId + "\n");
+				writer.write("\n");
+				System.out.println("Please insert link:");
+			    String userPost = in.nextLine();
+				writer.write(userPost + "\n");
+				writer.close();
+				in.close();			
+			} else {
+				System.out.println("Please insert 1 or 2");
+			}
+		} catch (IOException e) {
+			//e.printStackTrace();
+			System.out.println("Please insert 1 or 2");
+		}
+		Path path = Paths.get(".\\post.txt");
+		return path;
     }
 }
+
