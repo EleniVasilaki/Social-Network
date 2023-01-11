7d7d5966fe9f498aa7ea7d62652250973ea0c265
GeorgiaPapadopoulou <116336086+GeorgiaPapadopoulou@users.noreply.github.com>
2023-01-11 22:51:25 +0200

    Update Login.java

--- Login.java
+++ Login.java
@@ -3,40 +3,40 @@ import java.io.FileReader;
 import java.io.BufferedReader ;
 import java.io.File;
 import java.util.Scanner;
+import java.io.*;
 
 public class Login {
-	public static void logIn() {
-   		Scanner myobj1 = new Scanner(System.in);
-		System.out.println("Please enter your Username");
-    	String userName = myobj1.next();
+public static void login()throws FileNotFoundException, IOException {
+   	Scanner myobj1 = new Scanner(System.in);
+    System.out.println("Please enter your Username");
+    String userName = myobj1.next();
 
-    	Scanner myobj2 = new Scanner(System.in);
-    	System.out.println("Please enter your Password");
-    	String password = myobj2.next();
+    Scanner myobj2 = new Scanner(System.in);
+    System.out.println("Please enter your Password");
+    String password = myobj2.next();
 
-		String line = "";
-		BufferedReader br = null;
-
-		try {
-			FileReader file1 = new FileReader("users.txt");
-			BufferedReader br = new BufferedReader(new FileReader(file1));
-		} catch (FileNotFoundException e) {
-			System.err.println("Unable to open file " + file1);
+String line = "";
+	try {
+		BufferedReader br = new BufferedReader(new FileReader("users.txt"));
+	}catch (FileNotFoundException e) {
+		System.err.println("Unable to open file " + "users.txt");
+	}
+	BufferedReader br = new BufferedReader(new FileReader("users.txt"));
+	while ((line = br.readLine()) != null) {
+		String[] column = line.split(";");
+	try{
+		if ((userName.equals(column[1])) && (password.equals(column[2]))) {
+   	     System.out.println("Connected successfully! Welcome!");
+		}
+		else if ((userName.equals(column[1])) && (password != (column[2]))) {
+			System.out.println("Password incorrect");
+		}else if ((password.equals(column[1])) && (userName !=(column[2]))) {
+			System.out.println("Username incorrect");
 		}
 
-		while ((line = br.readLine()) != null) {
-			String[] column = line.split(";");
-			try {
-				if ((userName.equals(column[1])) && (password.equals(column[2]))) {
-   	     			System.out.println("Connected successfully! Welcome!");
-				} else if ((userName.equals(column[1])) && (password != (column[2]))) {
-					System.out.println("Password incorrect");
-				} else if ((password.equals(column[1])) && (userName !=(column[2]))) {
-					System.out.println("Username incorrect");
-				}
-			} catch (Exception e) {
-				System.err.println("error");
-			}
+	}catch (Exception e) {
+		 System.err.println("error");
 		}
 	}
+}
 }
