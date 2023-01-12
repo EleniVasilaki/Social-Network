201e9536fed6c14ea844be048d7fedbf1d902010
t8210103 <115114218+t8210103@users.noreply.github.com>
2023-01-12 13:11:57 +0200

    Fixed CreatePost

--- CreatePost.java
+++ CreatePost.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 // H upload na kanei extend thn create: mesw methodou ths create na kalw thn upload analoga ean ebale perigrafh h oxi o xrhsths????
 public  class CreatePost extends Post_code{
 	//public static String path;
-    public static void createPostMethod() { //return is for testing purposes
+	public void createPostMethod(String userID) {
         Scanner in = new Scanner(System.in);
         Random rand = new Random();
         StringBuilder buffer = new StringBuilder(10);
@@ -19,8 +19,9 @@ public  class CreatePost extends Post_code{
         	int limitedInt = 97 + (int) (rand.nextFloat() * (97 - 122));
         	buffer.append((char)  limitedInt);
         }
-        String randString = buffer.toString();
-        System.out.println("click 1 to add a description to your post" + "\n" + "click 2 to add text/link for your post"); // more options?
+        String postID = buffer.toString();
+        //The code above generates PostID
+        System.out.println("click 1 to add a description to your post" + "\n" + "click 2 to add text/link for your post");
         int option = in.nextInt();
         try {
 			BufferedWriter writer = new BufferedWriter( new FileWriter(".\\post.txt", true));
@@ -31,10 +32,11 @@ public  class CreatePost extends Post_code{
 				postId = postId + 1;
 			}
 			postId = postId / 3; //Otherwise postId goes up by 3 not by 1 */
-			//this code is for adding increasing numbers as an extra line of each post
+			//The hidden code above is for adding increasing numbers as an extra line of each post
 			in.nextLine();
 			if (option == 1) {
-				writer.write(randString + "\n");
+				writer.write(userID + "\n");
+				writer.write(postID + "\n");
 			    System.out.println("Please enter your description:");
 			    String description = in.nextLine();
 			    writer.write(description + "\n");
@@ -44,7 +46,8 @@ public  class CreatePost extends Post_code{
 				writer.close();
 				in.close();
 			} else if (option == 2) {
-				writer.write(randString + "\n");
+				writer.write(userID + "\n");
+				writer.write(postID + "\n");
 				writer.write("\n");
 				System.out.println("Please insert link:");
 			    String userPost = in.nextLine();
@@ -58,8 +61,7 @@ public  class CreatePost extends Post_code{
 			//e.printStackTrace();
 			System.out.println("Please insert 1 or 2");
 		}
-		Path path = Paths.get(".\\post.txt");
-	}
+    }
 }
 
 
