8591ddca51456382b7da2e19c08412450a9751ed
Jim Kodis <t8210224@aueb.gr>
2023-01-14 13:00:23 +0200

    Format Login.java
    Make it more readable

--- Login.java
+++ Login.java
@@ -6,38 +6,38 @@ import java.util.Scanner;
 import java.io.*;
 
 public class Login {
-public static String login()throws FileNotFoundException, IOException {
-   	Scanner myobj1 = new Scanner(System.in);
-    System.out.println("Please enter your Username");
-    String userName = myobj1.next();
+	public static String login() throws FileNotFoundException, IOException {
+   		Scanner myobj1 = new Scanner(System.in);
+    	System.out.println("Please enter your Username");
+    	String userName = myobj1.next();
 
-    Scanner myobj2 = new Scanner(System.in);
-    System.out.println("Please enter your Password");
-    String password = myobj2.next();
+    	Scanner myobj2 = new Scanner(System.in);
+    	System.out.println("Please enter your Password");
+    	String password = myobj2.next();
 
-String line = "";
-	try {
+		String line = "";
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("users.txt"));
+		} catch (FileNotFoundException e) {
+			System.err.println("Unable to open file " + "users.txt");
+		}
 		BufferedReader br = new BufferedReader(new FileReader("users.txt"));
-	}catch (FileNotFoundException e) {
-		System.err.println("Unable to open file " + "users.txt");
-	}
-	BufferedReader br = new BufferedReader(new FileReader("users.txt"));
-	while ((line = br.readLine()) != null) {
-		String[] column = line.split(";");
-	try{
-		if ((userName.equals(column[1])) && (password.equals(column[2]))) {
-   	     System.out.println("Connected successfully! Welcome!");
-		}
-		else if ((userName.equals(column[1])) && (password != (column[2]))) {
-			System.out.println("Password incorrect");
-		}else if ((password.equals(column[1])) && (userName !=(column[2]))) {
-			System.out.println("Username incorrect");
-		}
+		while ((line = br.readLine()) != null) {
+			String[] column = line.split(";");
+			try {
+				if ((userName.equals(column[1])) && (password.equals(column[2]))) {
+					System.out.println("Connected successfully! Welcome!");
+				} else if ((userName.equals(column[1])) && (password != (column[2]))) {
+					System.out.println("Password incorrect");
+				} else if ((password.equals(column[1])) && (userName !=(column[2]))) {
+					System.out.println("Username incorrect");
+				}
 
-	}catch (Exception e) {
-		 System.err.println("error");
+			} catch (Exception e) {
+		 		System.err.println("error");
+			}
 		}
-	}
-     return column[0];
-   }
+		return column[0];
+    }
 }
