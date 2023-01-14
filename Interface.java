4772c9ba530368bab76f01e64a7ac72e20010f4d
Jim Kodis <t8210224@aueb.gr>
2023-01-14 16:56:43 +0200

    Update Interface.java
    Remove constructor, comments and minor changes

--- Interface.java
+++ Interface.java
@@ -5,8 +5,6 @@ public class Interface {
     public static int option;
     public static String uid, pid;
 
-    public Interface() {}
-
     public void selection() {
         Scanner input = new Scanner(System.in);
         option = input.nextInt();
@@ -66,62 +64,12 @@ public class Interface {
 
             switch (option) {
                 case 1 -> Profile.changeProfile(uid);
-                case 2:
-                    pid = Post.myPostsMenu();
+                case 2 -> Post.myPostsMenu(uid);
                 case 3 -> mainMenu();
                 default -> System.out.println("Wrong input. Please try again");
             }
         } while (option != 1 & option != 2 & option != 3);
     }
-
-    /*
-    public void editProfileMenu() {
-        do {
-            System.out.println("""
-                    1.1.1 Change username
-                    1.1.2 Change password
-                    1.1.3 Change your bio
-                    1.1.4 Back
-                    """);
-
-            selection();
-
-            switch (option) {
-                case 1 -> Profile.changeUsername();
-                case 2 -> Profile.changePassword();
-                case 3 -> Profile.changeBio();
-                case 4 -> profileMenu();
-                default -> System.out.println("Wrong input. Please try again");
-            }
-        } while (option != 1 & option != 2 & option != 3 & option != 4);
-    }
-    */
-
-    /*
-    public void myPostsMenu() throws IOException {
-        do {
-            System.out.println("""
-                1.2.1 Edit
-                1.2.2 Delete
-                1.2.3 Next post
-                1.2.4 Back
-                """);
-
-            selection();
-
-            switch (option) {
-                case 1 -> Post.EditPostMethod();
-                case 2 -> Post.deletePostMethod(pid);
-                case 3 -> {
-                    Post.nextPost();
-                    feedMenu();
-                }
-                case 4 -> profileMenu();
-                default -> System.out.println("Wrong input. Please try again");
-            }
-        } while (option != 1 & option != 2 & option != 3 & option != 4);
-    }
-     */
 
     public void logOut() throws IOException {
         welcomeMenu();
