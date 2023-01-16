0ab4232ae24b138df40a249f93bb3cd630daced1
Jim Kodis <t8210224@aueb.gr>
2023-01-16 20:54:52 +0200

    Update SignUp.java
    Use static input from Interface

--- SignUp.java
+++ SignUp.java
@@ -51,7 +51,7 @@ public class SignUp {
 		}
 
 		System.out.println("Please enter a password");
-	    String password = input.next();
+	    String password = Interface.input.nextLine();
 
 		try {
 			File file = new File(path);
