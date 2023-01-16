58ad370d73a53020a83f22192d020c2dd313551f
t8210103 <115114218+t8210103@users.noreply.github.com>
2023-01-17 00:13:16 +0200

    FIxed feed and myposts

--- Post.java
+++ Post.java
@@ -76,60 +76,66 @@ public class Post {
     //Edit Method
 	
     public void editPostMethod(String userId, String postId){
+		
 	    try{
             ArrayList<String> editArrayList = new ArrayList<String>();
             LineNumberReader reader = new LineNumberReader(new InputStreamReader(new FileInputStream(".\\post.txt"), "UTF-8"));
 		    String line;
+			boolean flag = false;
             int i = 0;
-            while(line = reader.readLine()) != null) {
+            while((line = reader.readLine()) != null) {
        	        editArrayList.add(line);
        	    }
 			reader.close();
-            do{
-                if(userId==editArrayList.get(i) && postId==editArrayList.get(i+1)){
-                    System.out.println("1: Edit description \n" + "2: Edit link \n");
-                    Scanner scanner = new Scanner(System.in);
-                    int option = scanner.nextInt();
 
-                    if(option==1){
-                        System.out.println("Please enter new description for your post")
-                        String des = scanner.next();
+            do{
+				System.out.println(userId + " " + editArrayList.get(i));
+                if(userId.equals(editArrayList.get(i)) && postId.equals(editArrayList.get(i+1))){
+					flag = true;
+                    System.out.println("1: Edit description \n" + "2: Edit link \n");
+
+                    int option = Interface.input.nextInt();
+
+                    if(option == 1){
+                        System.out.println("Please enter new description for your post");
+                        String des = Interface.input.next();
                         editArrayList.set(i+2, des);
-                    } else if(option==2){
-                        System.out.println("Please enter new link for your post")
-                        String link = scanner.next();
+						Interface.mainMenu();
+                    } else if(option == 2){
+                        System.out.println("Please enter new link for your post");
+                        String link = Interface.input.next();
                         editArrayList.set(i+3, link);
+						Interface.mainMenu();
                     } else {
                         System.out.println("Wrong input. Please enter 1 or 2 \n");
+						throw new IOException();
                     }
-                    try {
-                        File oldfile = new File(".\\post.txt")
-                        File newfile = new File("newpost.txt");
-                        BufferedWriter writer = new BufferedWriter( new FileWriter("newpost.txt", true));
-                    
-                        for(String str: editArrayList) {
-                            writer.write(str + System.lineSeparator());
-                        }
-
-                        writer.close();
-                        oldfile.delete();
-                        File dump = new File(".\\post.txt");
-                        newfile.renameTo(dump);
-                        //System.out.println("New file created");
-
+					try {
+                    	File oldfile = new File(".\\post.txt");
+                    	File newfile = new File("newpost.txt");
+        	            BufferedWriter writer = new BufferedWriter( new FileWriter("newpost.txt", true));
+                
+                    	for(String str: editArrayList) {
+                        	writer.write(str + System.lineSeparator());
+                   		}
+                    	writer.close();
+                    	oldfile.delete();
+                    	File dump = new File(".\\post.txt");
+                    	newfile.renameTo(dump);
+                    	//System.out.println("New file created");
                     } catch (IOException e) {
-                        System.out.println("An error occurred.");
-                        e.printStackTrace();
-                    }
-               
-                }
-                 i = i + 4;
-            } while(editArrayList.get(i+4) != null && editArrayList.get(i)==userId && editArrayList.get(i+1)==postId)
+                    	System.out.println("An error occurred.");
+                    	e.printStackTrace();
+                	}
+				}
+                i = i + 4;
+            } while(editArrayList.get(i+4) != null || flag == true/*&& editArrayList.get(i) == userId && editArrayList.get(i+1) == postId */);
 
         } catch (IOException e) {
-            System.out.println("An error has occurred.");
-            e.printStackTrace();
+			editPostMethod(userId, postId);
         }
+		Post p = new Post();
+		p.myPostsMenu(userId);
     }
 	
 	//Delete Method
@@ -197,82 +203,169 @@ public class Post {
             System.out.println("An error has occurred.");
 
         }
+		Post p = new Post();
+		p.myPostsMenu(userId);
     }
 
 	//nextPost method
 	public void myPostsMenu(String userId) {
 
-		try{
-            
-			boolean first = true;
-		    List<String> postArrayList = new ArrayList<String>();
-		    LineNumberReader reader = new LineNumberReader(new InputStreamReader(new FileInputStream(".\\post.txt"), "UTF-8"));
-			// read entire line as string
-			String line = reader.readLine();
-			boolean flag = true;
+		try {
+//new
+			LineNumberReader reader = new LineNumberReader(new InputStreamReader(new FileInputStream(".\\post.txt"), "UTF-8"));
+        	ArrayList<String> listOfStrings = new ArrayList<String>();
 			int i = 0;
-			String postid;
-			String postdes;
-			String postlink;
+			String strLine;
+			boolean first = true;
+			boolean flag = false;
+			while((strLine = reader.readLine()) != null) {
+				listOfStrings.add(strLine);
+			}
+		 	listOfStrings.add(null);
+			
+			//Print first post
+			while(listOfStrings.get(i) != null && first == true) {
 
-			do {
-				// Arraylist with users posts
-				while (line != null) {
-					if (line == userId) {
-						for (int j = 0; j == 3; j++ ) {
-							postArrayList.add(line);
-							line = reader.readLine();
-						}
-					} else {
-						for (int k = 0; k == 3; k++ ) {
-							line = reader.readLine();
-						}
-					}	
+				if( listOfStrings.get(i).equals(userId) ) {
+					first = false;
+					System.out.println(listOfStrings.get(i + 2) + "\n" + listOfStrings.get(i + 3));
 				}
-
-				//show first post
-				if (first) {
-					if (userId == postArrayList.get(i)) {
-						postid = postArrayList.get(i + 1);
-						postdes = postArrayList.get(i + 2);
-						postlink = postArrayList.get(i + 3);
-
-						System.out.println(postdes + "\n" + postlink);
-						first = false;
-					}
-				}
-				System.out.println("1. Edit post \n" + "2. Delete post \n" + "3. Next Post \n" + "4. Go back to profile \n");
-				Scanner scanner = new Scanner(System.in);
-				int answer = scanner.nextInt();
-
-				if (answer == 1) {
-					EditPostMethod(userId, postid);
-				} else if (answer == 2) {
-					deletePostMethod(userId, postid);
-				} else if (answer == 3) {
-					i = i + 4;
-					postid = postArrayList.get(i + 1);
-					postdes = postArrayList.get(i + 2);
-					postlink = postArrayList.get(i + 3);
-
-					System.out.println(postdes + "\n" + postlink);
-
-						if (postArrayList.get(i + 4) == null) {       					
-       					System.out.println("You have reached the end of your posts\n" + "Sending you back to your first post \n");
-       					first = true;
-						i = 0;
-       					} 
-				} else if(answer==4) { //Go back
-					Interface.profileMenu();
+				i += 4;
+			}
+			if (first == false) {
+				do {
 					flag = false;
-				} else { //wrong input
-					System.out.println("Wrong input. Please enter 1, 2, 3 or 4 \n");
-				}
+					System.out.println("1. Edit post \n" + "2. Delete post \n" + "3. Next Post \n" + "4. Go back to profile \n");
+					int answer = Interface.input.nextInt();
+					if (answer == 1) {
+						editPostMethod(userId, listOfStrings.get(i + 1));
+					} else if (answer == 2) {
+						deletePostMethod(userId, listOfStrings.get(i + 1));
+					} else if (answer == 3) {
 
-			} while (postArrayList.get(i + 4) != null && flag == true);
-			reader.close();
+						while(listOfStrings.get(i) != null && flag == false) {
+
+							if( listOfStrings.get(i).equals(userId) ) {
+								System.out.println(listOfStrings.get(i + 2) + "\n" + listOfStrings.get(i + 3));
+								flag = true;
+							}
+							i += 4;
+						}
+						if (listOfStrings.get(i) == null) {
+							System.out.println("You have reached the end of your posts\n" + "Sending you back to your first post \n");
+							myPostsMenu(userId);
+						}
+					} else if (answer == 4) {
+						Interface.profileMenu();
+					} else {
+						System.out.println("Wrong input. Please enter 1, 2, 3 or 4 \n");
+						myPostsMenu(userId);
+					}
+				} while (flag == true);
+				
+			} else {
+				System.out.println("You dont have any posts");
+				try {
+					Interface.profileMenu();
+				} catch (IOException e) {
+				}
+			}
+
 		} catch (Exception e) {
-			System.out.println("An error has occurred.");
+
 		}
 	}
 }
+// new
+		/*try{
+            
+		    List<String> postArrayList = new ArrayList<String>();
+		    LineNumberReader reader = new LineNumberReader(new InputStreamReader(new FileInputStream(".\\post.txt"), "UTF-8"));
+			// read entire line as string
+			String line = null;
+			boolean first = false; //Becomes true if user has any posts
+			boolean flag = true;
+			int i = 0;
+			String postid = null;
+			String postdes;
+			String postlink;
+
+			// Arraylist with users posts
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+				System.out.println(userId);
+				if (line == userId) {
+					
+					for (int j = 0; j == 3; j++ ) {
+						postArrayList.add(line);
+						line = reader.readLine();
+						first = true; //Tests if user has any posts at all
+					}
+				} else {
+
+					for (int k = 0; k == 3; k++ ) {
+						line = reader.readLine();
+					}
+				}	
+			}
+			postArrayList.add(line); //So i can check if there are no more posts (I need the last shell to be empty)
+
+			if (first == true) {
+				do {
+					//show first post
+					if (first == true) { //First now has another use, it allows the commands below to exeute only once
+						if (userId == postArrayList.get(i)) {
+							postid = postArrayList.get(i + 1);
+							postdes = postArrayList.get(i + 2);
+							postlink = postArrayList.get(i + 3);
+	
+							System.out.println(postdes + "\n" + postlink);
+							first = false;
+						}
+					}
+					System.out.println("1. Edit post \n" + "2. Delete post \n" + "3. Next Post \n" + "4. Go back to profile \n");
+					int answer = Interface.input.nextInt();
+	
+					if (answer == 1) {
+						editPostMethod(userId, postid);
+					} else if (answer == 2) {
+						deletePostMethod(userId, postid);
+					} else if (answer == 3) {
+
+						if (postArrayList.get(i + 4) == null) {       					
+							System.out.println("You have reached the end of your posts\n" + "Sending you back to your first post \n");
+							myPostsMenu(userId);
+						} else {
+							i = i + 4;
+							postid = postArrayList.get(i + 1);
+							postdes = postArrayList.get(i + 2);
+							postlink = postArrayList.get(i + 3);
+		
+							System.out.println(postdes + "\n" + postlink);
+						}
+
+					} else if(answer==4) { //Go back
+						Interface.profileMenu();
+						flag = false;
+					} else { //wrong input
+						System.out.println("Wrong input. Please enter 1, 2, 3 or 4 \n");
+						myPostsMenu(userId);
+					}
+	
+				} while (postArrayList.get(i + 4) != null && flag == true);
+				reader.close();
+
+			} else {
+				System.out.println("You have no posts");
+			}
+
+		} catch (Exception e) {
+			System.out.println("An error has occurred.");
+		}
+		try {
+			Interface.profileMenu();
+		} catch (IOException e) {
+
+		}
+	}
+} */
