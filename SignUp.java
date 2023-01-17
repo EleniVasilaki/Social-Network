1c46373a156d03318744096299b12b5838ef1fbc
t8210103 <115114218+t8210103@users.noreply.github.com>
2023-01-17 14:42:28 +0200

    Txts filled

--- SignUp.java
+++ SignUp.java
@@ -8,7 +8,7 @@ import java.io.FileOutputStream;
 import java.util.Scanner;
 
 public class SignUp {
-
+	static int id = 1;
 	public String signUp() {
 
 		String path = "users.txt";
@@ -17,14 +17,14 @@ public class SignUp {
 		boolean x = true;
 		String username ="";
 		String line;
-		int id = 1;
+		
 		username = Interface.input.next();
 
 		try {
 
 			do
 			{
-				id = 1;
+				
 				FileReader fr = new FileReader("users.txt");
 				BufferedReader br = new BufferedReader(fr);
 				
@@ -52,6 +52,7 @@ public class SignUp {
 
 		System.out.println("Please enter a password");
 	    String password = Interface.input.next();
+		id += 1;
 
 		try {
 			File file = new File(path);
